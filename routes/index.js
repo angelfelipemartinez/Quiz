fc58fc61a6d4b19cbbea7d1b8c26357a0f6f6e41
angelfelipemartinez <angelfelipemartinez@gmail.com>
2015-08-08 18:07:35 +0200

    Añadir comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,7 +14,7 @@ router.get('/author/author', function(req,res){
 	res.render('author/author', {title: 'Autor', errors:[]});
 });
 
-//Autoload de comandos con :quizÍd
+//Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -24,5 +25,8 @@ router.post('/quizes/create',			   quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',	   	   quizController.update);
 router.delete('/quizes/:quizId(\\d+)',	   quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',	  commentController.create);
 
 module.exports = router;
